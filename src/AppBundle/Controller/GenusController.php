87759f38b3d27e86bd240dbdc68adec37e7314fe
mehdi kabbaj <mka@octo.com>
2017-02-18 22:41:03 +0000

    Create New Layout

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -20,14 +20,15 @@ class GenusController extends Controller
      */
     public function showAction($genusName)
     {
-
-        $templating = $this->container->get('templating');
-        $html = $templating->render('genus/show.html.twig', array(
-            'name' => $genusName
+        $notes = [
+            'Octopus asked me a riddle, outsmarted me',
+            'I counted 8 legs... as they wrapped around me',
+            'Inked!'
+        ];
+        return $this->render('genus/show.html.twig', array(
+            'name' => $genusName,
+            'notes' => $notes
         ));
-
-        return new Response($html);
-
     }
 
 }
